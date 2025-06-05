f41ae038e448004ff7cac842e265c8aa587c62c8
YooSejeong <nived3@naver.com>
2025-06-05 12:04:44 +0900

    chore: 누락된 Lombok 어노테이션 추가

--- src/main/java/com/studycrew/studyBoard/entity/StudyApplication.java
+++ src/main/java/com/studycrew/studyBoard/entity/StudyApplication.java
@@ -7,8 +7,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
 public class StudyApplication {
 
     @Id
