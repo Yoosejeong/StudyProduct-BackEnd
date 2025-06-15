2950492556c6f7cb2d9eb97b7bb5c8077e78376e
YooSejeong <nived3@naver.com>
2025-06-15 20:20:31 +0900

    feat: StudyPost와 User 기준으로 중복 지원 여부 확인하는 existsByStudyPostAndUser 메서드 추가

--- src/main/java/com/studycrew/studyBoard/repository/StudyApplicationRepository.java
+++ src/main/java/com/studycrew/studyBoard/repository/StudyApplicationRepository.java
@@ -1,9 +1,12 @@
 package com.studycrew.studyBoard.repository;
 
 import com.studycrew.studyBoard.entity.StudyApplication;
+import com.studycrew.studyBoard.entity.StudyPost;
+import com.studycrew.studyBoard.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudyApplicationRepository extends JpaRepository<StudyApplication, Long> {
+    boolean existsByStudyPostAndUser(StudyPost studyPost, User user);
 }
