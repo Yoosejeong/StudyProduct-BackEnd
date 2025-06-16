641745a1269544f9023814d83611e2f0c0b149d8
YooSejeong <nived3@naver.com>
2025-06-17 01:11:37 +0900

    feat: StudyApplication → MyStudyApplicationResponse 변환 메서드 컨버터에 추가

--- src/main/java/com/studycrew/studyBoard/converter/StudyApplicationConverter.java
+++ src/main/java/com/studycrew/studyBoard/converter/StudyApplicationConverter.java
@@ -1,5 +1,6 @@
 package com.studycrew.studyBoard.converter;
 
+import com.studycrew.studyBoard.dto.StudyApplicationDTO.StudyApplicationResponseDTO.MyStudyApplicationResponse;
 import com.studycrew.studyBoard.dto.StudyApplicationDTO.StudyApplicationResponseDTO.StudyApplicationListResponse;
 import com.studycrew.studyBoard.dto.StudyApplicationDTO.StudyApplicationResponseDTO.StudyApplicationResult;
 import com.studycrew.studyBoard.entity.StudyApplication;
@@ -31,6 +32,16 @@ public class StudyApplicationConverter {
                 .appliedAt(studyApplication.getCreatedAt())
                 .userId(studyApplication.getUser().getId())
                 .nickname(studyApplication.getUser().getNickname())
+                .build();
+    }
+
+    public static MyStudyApplicationResponse toUserApplications(StudyApplication studyApplication) {
+        return MyStudyApplicationResponse.builder()
+                .applicationStatus(studyApplication.getApplicationStatus())
+                .appliedAt(studyApplication.getCreatedAt())
+                .studyPostId(studyApplication.getStudyPost().getId())
+                .studyApplicationId(studyApplication.getId())
+                .studyTitle(studyApplication.getStudyPost().getTitle())
                 .build();
     }
 }
