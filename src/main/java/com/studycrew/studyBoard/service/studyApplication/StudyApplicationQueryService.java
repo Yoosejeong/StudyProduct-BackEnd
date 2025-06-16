c6012255cab27bd144d071041f5f42982f27ae0a
YooSejeong <nived3@naver.com>
2025-06-16 20:39:44 +0900

    feat: 회원이 지원한 스터디 목록 서비스 추가

--- src/main/java/com/studycrew/studyBoard/service/studyApplication/StudyApplicationQueryService.java
+++ src/main/java/com/studycrew/studyBoard/service/studyApplication/StudyApplicationQueryService.java
@@ -1,8 +1,10 @@
 package com.studycrew.studyBoard.service.studyApplication;
 
+import com.studycrew.studyBoard.dto.StudyApplicationDTO.StudyApplicationResponseDTO.MyStudyApplicationResponse;
 import com.studycrew.studyBoard.dto.StudyApplicationDTO.StudyApplicationResponseDTO.StudyApplicationListResponse;
 import java.util.List;
 
 public interface StudyApplicationQueryService {
     List<StudyApplicationListResponse> findAllApplicants(Long studyPostId);
+    List<MyStudyApplicationResponse> findMyStudyApplications(Long userId);
 }
--- src/main/java/com/studycrew/studyBoard/service/studyApplication/StudyApplicationQueryServiceImpl.java
+++ src/main/java/com/studycrew/studyBoard/service/studyApplication/StudyApplicationQueryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.studycrew.studyBoard.service.studyApplication;
 
 import com.studycrew.studyBoard.converter.StudyApplicationConverter;
+import com.studycrew.studyBoard.dto.StudyApplicationDTO.StudyApplicationResponseDTO.MyStudyApplicationResponse;
 import com.studycrew.studyBoard.dto.StudyApplicationDTO.StudyApplicationResponseDTO.StudyApplicationListResponse;
 import com.studycrew.studyBoard.entity.StudyApplication;
 import com.studycrew.studyBoard.repository.StudyApplicationRepository;
@@ -23,5 +24,11 @@ public class StudyApplicationQueryServiceImpl implements StudyApplicationQuerySe
         return studyApplicationList.stream()
                 .map(StudyApplicationConverter::toApplicationList)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<MyStudyApplicationResponse> findMyStudyApplications(Long userId) {
+        studyApplicationRepository.findByUserId(userId);
+        return List.of();
     }
 }
