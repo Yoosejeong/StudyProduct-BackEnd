8f00e4c855b2ccb36f6336473bff848ca6f4c1db
YooSejeong <nived3@naver.com>
2025-06-18 18:10:24 +0900

    feat: SwaggerConfig 추가

---
+++ src/main/java/com/studycrew/studyBoard/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.studycrew.studyBoard.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Study API")
+                        .version("v1.0")
+                        .description("스터디 모집 플랫폼 API 명세서"))
+                .components(new Components()
+                        .addSecuritySchemes("BearerToken", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .security(List.of(new SecurityRequirement().addList("BearerToken")));
+    }
+}
