7f9aca981f1be1da9f59021f944682a7debf7480
YooSejeong <nived3@naver.com>
2025-06-18 18:09:42 +0900

    chore: Swagger 경로 permitAll 처리

--- src/main/java/com/studycrew/studyBoard/config/SecurityConfig.java
+++ src/main/java/com/studycrew/studyBoard/config/SecurityConfig.java
@@ -84,7 +84,7 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/api/login", "/", "/api/signUp","/api/reissue").permitAll()
+                        .requestMatchers("/api/login", "/", "/api/signUp","/api/reissue","/v3/api-docs/**","/swagger-ui/**","/swagger-ui.html").permitAll()
                         .requestMatchers("/api/user").hasRole("USER")
                         .anyRequest().authenticated());
 
