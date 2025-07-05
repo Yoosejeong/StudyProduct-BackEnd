ca7ea0015b611edc285d7bd1dd6e0790054a13d1
YooSejeong <nived3@naver.com>
2025-07-05 21:16:10 +0900

    feat: StudyPost와 ApplicationStatus로 StudyApplication 리스트 조회 메서드 추가

--- src/main/java/com/studycrew/studyBoard/repository/StudyApplicationRepository.java
+++ src/main/java/com/studycrew/studyBoard/repository/StudyApplicationRepository.java
@@ -3,6 +3,7 @@ package com.studycrew.studyBoard.repository;
 import com.studycrew.studyBoard.entity.StudyApplication;
 import com.studycrew.studyBoard.entity.StudyPost;
 import com.studycrew.studyBoard.entity.User;
+import com.studycrew.studyBoard.enums.ApplicationStatus;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,4 +13,5 @@ public interface StudyApplicationRepository extends JpaRepository<StudyApplicati
     boolean existsByStudyPostAndUser(StudyPost studyPost, User user);
     List<StudyApplication> findByStudyPostId(Long studyPostId);
     List<StudyApplication> findByUserId(Long userId);
+    List<StudyApplication> findAllByStudyPostAndApplicationStatus(StudyPost studyPost, ApplicationStatus applicationStatus);
 }
